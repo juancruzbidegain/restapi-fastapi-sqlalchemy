28711e6eca0d429a4c34201c7d10f49facea034f
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2022-01-23 20:05:10 -0300

    add route to post user

--- routes/user.py
+++ routes/user.py
@@ -7,9 +7,34 @@ from models.user import users
 #sqlalchemy
 from sqlalchemy import select, func
 
+
+#schemas
+from schemas.user  import User
+
+
+
+from cryptography.fernet import Fernet
+
+
 user = APIRouter()
+
+
+key = Fernet.generate_key()
+f = Fernet(key)
 
 @user.get('/users')
 def get_users():
-    #return "Hola" 
-    return con.execute(users.select().fetch_all())
+    return con.execute(users.select()).fetchall()
+
+
+@user.post('/users')
+def create_users(user: User):
+    new_user = dict(user)
+    new_user["password"] = f.encrypt(user.password.encode("utf-8"))
+    execute_insert = con.execute(users.insert().values(new_user))
+    print(execute_insert.lastrowid)
+    return con.execute(users.select().where(users.c.id == execute_insert.lastrowid)).first()
+
+@user.get('/users/{id}')
+def get_user(id : str):
+    return con.execute(users.select().where(users.c.id == id)).first()
